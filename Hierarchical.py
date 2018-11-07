0b8a1a957f9a012be309002860c07214811ecf20
aleks73337 <aleks73337@gmail.com>
2018-11-07 11:33:49 +0300

    commit

---
+++ Hierarchical.py
@@ -0,0 +1,42 @@
+from scipy.cluster.hierarchy import *
+from scipy.spatial.distance import pdist
+from sklearn import mixture
+from sklearn.decomposition import PCA
+import pandas as pd
+from pylab import *
+import matplotlib.pyplot as mpl
+from mpl_toolkits.mplot3d import Axes3D
+
+data = pd.read_csv('quake.csv', sep=',')
+data = data.values
+for i in range(data.shape[0]):
+    data[i,2] = data[i,2] + 150
+    data[i,2] = (data[i,2] + 150) % 300 - 150
+
+data_dist = pdist(data, 'euclidean')
+data_linkage = linkage(data_dist, method='ward')
+flat_clusters = fcluster(data_linkage, 5, 'maxclust')
+
+fig = mpl.figure()
+ax = fig.add_subplot(111, projection='3d')
+colors = []
+for i in range(len(flat_clusters)): #перечисление используемых цветов (костыль для белых точек на белом фоне)
+    if (flat_clusters[i] == 0):
+        colors.append('red')
+    if (flat_clusters[i] == 1):
+        colors.append('brown')
+    if (flat_clusters[i] == 2):
+        colors.append('green')
+    if (flat_clusters[i] == 3):
+        colors.append('orange')
+    if (flat_clusters[i] == 4):
+        colors.append('black')
+    if (flat_clusters[i] == 5):
+        colors.append('coral')
+    if (flat_clusters[i] == 6):
+        colors.append('blue')
+    if (flat_clusters[i] == 7):
+        colors.append('purple')
+ax.scatter(data[:,1],data[:,2], data[:,0], color = colors, alpha=1.0 , cmap=mpl.hot())
+#mpl.savefig('k-means 5 clusters')
+mpl.show()
